e2f7afe6989f58874716fea3e1e6e5de3d8da5ca
Matias Gonzalez Tognoni <matias@arkitektum.no>
2018-10-11 11:19:56 +0200

    Update Unit Test

--- dmnClient.Test/ExcelToDmnTests.cs
+++ dmnClient.Test/ExcelToDmnTests.cs
@@ -23,24 +23,28 @@ namespace dmnClient.Test
 
             var file = "dmnTest1.xlsx";
             var savedFilePath = Path.Combine(Directory.GetCurrentDirectory() + @"..\..\..\..\TestData\", file);
+            //var savedFilePath = @"C:\Users\MatiasGonzalezTognon\Dropbox (Arkitektum AS)\Ark_prosjekter\DIBK\DigiTek\Brannteknisk prosjektering\DMN\Trinn1\ExcelFiles\13_OverflateKledning.xlsx";
             var name = Path.GetFileNameWithoutExtension(savedFilePath);
             var fi = new FileInfo(savedFilePath);
-
             ExcelPackage ep = new ExcelPackage(new FileInfo(savedFilePath));
+
             ExcelPackage ep1 = new ExcelPackage(resourceAsStream);
 
             //ExcelPackage ep = new ExcelPackage(new FileInfo(resourcePath));
             ExcelWorksheet workSheet = ep.Workbook.Worksheets.FirstOrDefault();
-            ExcelWorksheet workSheet1 = ep1.Workbook.Worksheets.FirstOrDefault();
-            var inputIndex = new[] { "B", "D", "C", "E" };
-            var outputIndex = new[] { "F", "G", "H" };
+            var table = ep1.Workbook.Worksheets.FirstOrDefault().Tables.FirstOrDefault();
 
-            var outputsRulesDictionary = new ExcelServices().GetRulesFromExcel(workSheet, outputIndex, true);
-            var inputsRulesDictionary = new ExcelServices().GetRulesFromExcel(workSheet, inputIndex, true);
-            var outputsRulesTypes = new ExcelServices().GetRulesTypes(workSheet, outputIndex, true);
-            var inputsRulesTypes = new ExcelServices().GetRulesTypes(workSheet, inputIndex, true);
-            var inputsDictionary = new ExcelServices().GetExcelHeaderName(workSheet, inputIndex, true);
-            var outputsDictionary = new ExcelServices().GetExcelHeaderName(workSheet, outputIndex, true);
+            var columnsDictionary = ExcelServices.GetColumnRagngeInLeters(table, 4, 3);
+            columnsDictionary.TryGetValue("outputsIndex", out var outputsIndex);
+            columnsDictionary.TryGetValue("inputsIndex", out var inputsIndex);
+
+
+            var outputsRulesDictionary = new ExcelServices().GetRulesFromExcel(workSheet, outputsIndex, true);
+            var inputsRulesDictionary = new ExcelServices().GetRulesFromExcel(workSheet, inputsIndex, true);
+            var outputsRulesTypes = new ExcelServices().GetRulesTypes(workSheet, outputsIndex, true);
+            var inputsRulesTypes = new ExcelServices().GetRulesTypes(workSheet, inputsIndex, true);
+            var inputsDictionary = new ExcelServices().GetExcelHeaderName(workSheet, inputsIndex, true);
+            var outputsDictionary = new ExcelServices().GetExcelHeaderName(workSheet, outputsIndex, true);
             var dmnInfo = new ExcelServices().GetDmnInfo(workSheet).FirstOrDefault();
             var dmnName = dmnInfo.Value;
             var dmnId = dmnInfo.Key;
